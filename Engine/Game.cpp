7a50ef295f68b30af199ef643569933b956caad9
youns <youns.s.ibrahim115@gmail.com>
2022-10-26 22:51:34 +0300

    move crosshair on screen(test)

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -21,6 +21,7 @@
 #include "MainWindow.h"
 #include "Game.h"
 
+
 Game::Game( MainWindow& wnd )
 	:
 	wnd( wnd ),
@@ -42,4 +43,68 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
+	int x = 400, y = 300;
+	int r = 255, g = 255, b = 255;
+	const int w_key = 0x57, a_key= 0x41, s_key = 0x53, d_key= 0x44;
+
+	//color
+	if (wnd.kbd.KeyIsPressed(VK_SPACE)) {
+		g = 165;
+		b = 0;
+		
+	}
+	
+	//postiion
+	if (wnd.kbd.KeyIsPressed(w_key)) {
+		y = y - 100;
+	}
+	if (wnd.kbd.KeyIsPressed(a_key)) {
+		x = x - 100;
+	}
+	if (wnd.kbd.KeyIsPressed(s_key)) {
+		y = y + 100;
+	}
+	if (wnd.kbd.KeyIsPressed(d_key)) {
+		x = x + 100;
+	}
+	
+	//shape
+	if (wnd.kbd.KeyIsPressed(VK_SHIFT)) {
+		gfx.PutPixel(x-105, y, r, g, b);
+		gfx.PutPixel(x-104, y, r, g, b);
+		gfx.PutPixel(x-103, y, r, g, b);
+					   
+		gfx.PutPixel(x+103, y, r, g, b);
+		gfx.PutPixel(x+104, y, r, g, b);
+		gfx.PutPixel(x+105, y, r, g, b);
+
+		gfx.PutPixel(x, y-105, r, g, b);
+		gfx.PutPixel(x, y-104, r, g, b);
+		gfx.PutPixel(x, y-103, r, g, b);
+		gfx.PutPixel(x, y, r, g, b);
+					
+		gfx.PutPixel(x, y+103, r, g, b);
+		gfx.PutPixel(x, y+104, r, g, b);
+		gfx.PutPixel(x, y+105, r, g, b);
+
+	}
+	else {
+		gfx.PutPixel(x - 5, y, r, g, b);
+		gfx.PutPixel(x - 4, y, r, g, b);
+		gfx.PutPixel(x - 3, y, r, g, b);
+
+		gfx.PutPixel(x + 3, y, r, g, b);
+		gfx.PutPixel(x + 4, y, r, g, b);
+		gfx.PutPixel(x + 5, y, r, g, b);
+
+		gfx.PutPixel(x, y - 5, r, g, b);
+		gfx.PutPixel(x, y - 4, r, g, b);
+		gfx.PutPixel(x, y - 3, r, g, b);
+		
+
+		gfx.PutPixel(x, y + 3, r, g, b);
+		gfx.PutPixel(x, y + 4, r, g, b);
+		gfx.PutPixel(x, y + 5, r, g, b);
+	}
+
 }
